91e23421e1aaf7f3213defebf5b6c626347dc2fd
shailesh5050 <shaileshkuswaha1@gmail.com>
2024-10-23 07:41:23 +0530

    Added : User Controller Config

--- src/app.js
+++ src/app.js
@@ -10,15 +10,17 @@ app.use(
     credentials: true,
   })
 );
-
 app.use(cookieParser());
-
+app.use(express.static('public'));
 app.use(express.json());
-
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
+
+//Route Declarations
+import userRoute from './routes/user.routes.js';
+app.use('/api/v1/user', userRoute);
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
 import express from 'express';
-
+import { app } from './app.js';
 dotenv.config({
   path: './env',
 });
-
-const app = express();
 
 app.use(express.json());
 
